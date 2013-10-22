7154d3717fa6439d236a94da49ebbedd64e61238
Kevin Brown <kevin.s.brown@uconn.edu>
2013-10-22 14:35:51 -0400

    Initial Commit

---
+++ NLPFlow/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NLPFlow
+//
+//  Created by Kevin Brown on 10/22/13.
+//  Copyright (c) 2013 Kevin Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
