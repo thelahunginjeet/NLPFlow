8960bfba0148787fce94f85d383292b2ed303256
Kevin Brown <kevin.s.brown@uconn.edu>
2013-11-18 15:47:28 -0500

    Added Parameter class.

---
+++ NLPFlow/Source/NLPParameter.cpp
@@ -0,0 +1,9 @@
+//
+//  NLPParameter.cpp
+//  NLPFlow
+//
+//  Created by Kevin Brown on 11/18/13.
+//  Copyright (c) 2013 Kevin Brown. All rights reserved.
+//
+
+#include "NLPParameter.h"
---
+++ NLPFlow/Source/NLPParameter.hpp
@@ -0,0 +1,14 @@
+//
+//  NLPParameter.h
+//  NLPFlow
+//
+//  Created by Kevin Brown on 11/18/13.
+//  Copyright (c) 2013 Kevin Brown. All rights reserved.
+//
+
+#ifndef __NLPFlow__NLPParameter__
+#define __NLPFlow__NLPParameter__
+
+#include <iostream>
+
+#endif /* defined(__NLPFlow__NLPParameter__) */
